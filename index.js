adc2491fca1784663eb172017a22fd7d51d05b19
Faithlucy <cecygachanja5@gmail.com>
2023-11-06 16:35:08 +0300

    First js Commit

--- index.js
+++ index.js
@@ -1,23 +1,4 @@
-const baseURL= "http://localhost:3000/patients"
-
-//diplay patient details
-const searchdata= document.getElementById("searchbutton")
-searchdata.addEventListener('click', (e)=>{
-    e.preventDefault()
-    fetch(baseURL)
-    .then((response)=> response.json())
-    .then((data)=>{
-        const displaydata= document.getElementById("display")
-        data.forEach(element => {
-            const newp = document.createElement('p');
-            newp.textContent=`Name : ${element.first_name} medical history :${element.medical_history}`;
-            displaydata.appendChild(newp)
-            });     
-        
-
-
-        
-    })
-})
-
-
+const baseURL="http://localhost:3000/patients"
+const inputSearch=document.getElementById("search")
+const searchButton=document.getElementById("searchbutton")
+const displaydetails=document.getElementsById("display")
