85ad774bc567c1d511880e62ad559a798ba9ddb7
Faithlucy <cecygachanja5@gmail.com>
2023-11-06 17:17:28 +0300

    Detailed README.md

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # phase-1-project
+# Project Title: "MedHistory"
+## Description
+This project aims to create an application that will allow users to search for their previous medical history,including past medication and related family history.
+This project is a web application that allows users to create and update their medical history
+## Features
+The features of the project include:
+- Search button
+- adding patients.
+- updating patient's information.
+- deleting patient's information.
+
+## Project SetUp instructions
+To set up this project, you need to have NodeJS installed on your computer. You can download it from https://nodejs.org/en.
+
+## Link to Live site 
+git@github.com:Faithlucy/phase-1-project.git
+
--- index.js
+++ index.js
@@ -7,6 +7,38 @@ fetch(baseURL)
 .then((response)=> response.json())
 .then((data)=>{
     searchButton.addEventListener("click",
-    ()=>{}
+    ()=>{
+        if(inputSearch.textContenT===data.first_name){
+            alert(`${data.name} found`)
+        }else{
+            alert('Patient not Found')
+        }
+    }
         
     )})
+
+    const submit= document.getElementById("submit")
+    const patientname = document.getElementById("fname").value;
+    submit.addEventListener("submit",()=>{
+
+
+        fetch(baseURL, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify()
+
+            
+                
+                
+                
+          })
+          .then(response => response.json())
+          .then(data => console.log(data))
+          .catch(error => console.error(error))
+
+    } )
+
+
+    
